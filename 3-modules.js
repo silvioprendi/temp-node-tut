0689d24d600beb605d191b8f0a9a5cafe9e6498d
Silvio Prendi <sprendi20@epoka.edu.al>
2022-07-14 16:32:43 +0200

    First commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utilis')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+// console.log(data)
+
+// sayHi(names.silvio)
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'Super secret'
+// share
+const john = 'john'
+const silvio = 'silvio'
+
+
+module.exports = {john, silvio}
