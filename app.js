b15eed8a1cbcedf10ef3df77a7545bd0755cbcb0
Silvio Prendi <sprendi20@epoka.edu.al>
2022-07-14 21:14:10 +0200

    Second Commit

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const http = require('http');
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const server = http.createServer((req, res)=> {
-    if(req.url === '/' ) {
-        res.end('Welcome to our home page')
-    }
-    if(req.url === '/about') {
-        res.end('This is a short story')
-    }
-    res.end(`
-        <h1>Oops!</h1>
-        <p>We can't seem to find the page you are looking for</p>
-        <a href="/">back home </a>
-    `)
-})
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `This is AWESOME : ${first} ${second}`,
+      {flag: 'a'}
+    );
+    console.log(first, second);
+  } catch (err) {
+    console.log(error);
+  }
+};
 
-server.listen(5000)
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
